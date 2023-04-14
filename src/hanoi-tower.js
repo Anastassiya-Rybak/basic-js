d1b0b38dadc0b5e6a4ea6bbf8c2757a738d8593a
Anastassiya <vyacheslavovna.an@mail.ru>
2023-04-14 15:22:15 +0600

    feat: hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,10 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  let res = {
+    turns: 0,
+    seconds: 0
+  };
+  res.turns = Math.floor((2 ** disksNumber) - 1);
+  console.log(res.turns);
+  let turnForSec = (turnsSpeed / 60) / 60;
+  res.seconds = Math.floor(res.turns / turnForSec);
+  return res;
 }
+
+console.log(calculateHanoi(5, 4074));
 
 module.exports = {
   calculateHanoi
--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+
+  if (typeof date !== 'object' || date === null || Array.isArray(date)) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if(isNaN(Date.parse(date.toString())) || Date.parse(date.toString()) > Date.parse(new Date())){
+    return 'Invalid date!';
+  }
+
+  if (!(date instanceof Date) || isNaN(date) || Object.prototype.toString.call(date) !== '[object Date]')  {
+    return 'Invalid date!';
+  }
+
+  let month = date.getMonth();
+  if (typeof month == 'number'){
+    switch (month) {
+      case 11 :
+        return 'winter';
+      case 0 :
+        return 'winter';
+      case 1 :
+        return 'winter';
+      case 2 :
+        return 'spring';      
+      case 3 :
+        return 'spring';
+      case 4 :
+        return 'spring';
+      case 5 :
+        return 'summer';
+      case 6 :
+        return 'summer';      
+      case 7 :
+        return 'summer';
+      case 8 :
+        return 'autumn';
+      case 9 :
+        return 'autumn';      
+      case 10 :
+        return 'autumn';
+    }
+  }else{
+    return 'Invalid date!';
+  }
 }
+
+console.log(getSeason(55));
 
 module.exports = {
   getSeason
