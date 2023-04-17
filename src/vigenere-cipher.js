697766a762992554b07db31635c57501d2c2e80a
Anastassiya <vyacheslavovna.an@mail.ru>
2023-04-18 00:29:12 +0600

    feat: not full vigenere task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,7 +21,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class VigenereCipheringMachine {
   encrypt(str, key) {
-    if (str === null || key === null){throw new Error('Incorrect arguments!')};
+    if (str == null || key == null){throw new Error('Incorrect arguments!')};
     const cipherK = {
       0: 'A',
       1: 'B',
@@ -52,27 +52,81 @@ class VigenereCipheringMachine {
     };
     let encryptedInNums = [];
     let encryptedKey = [];
+    let encryptedInSum = [];
 
     for (const letter of key) {
-      for (let i = 0; i < 26; i++) {
-        if (letter == cipherK[i]) {encryptedKey.push(cipherK[i]);}
-        else{encryptedKey.push(letter)};
+      try {
+        for (let i = 0; i < 26; i++) {
+          if (letter.toUpperCase() === cipherK[i]) {encryptedKey.push(i); break}
+        }
+      } catch {
+        encryptedKey.push(letter);
       }
-    }
+    };
+
+    let objKey = Object.keys(cipherK);
+    let regexSymbol = /[!\"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~]/;
 
     for (const letter of str) {
-      for (let i = 0; i < 26; i++) {
-        if (letter == cipherK[i]) {encryptedInNums.push(cipherK[i]);}
-        else{encryptedInNums.push(letter)};
+      if(letter == ' '){encryptedInNums.push(letter)};
+      if (regexSymbol.test(letter)){encryptedInNums.push(letter)};
+      
+      try {
+        for (let i = 0; i < 26; i++) {
+          if (letter.toUpperCase() === cipherK[i]) {encryptedInNums.push(objKey[i]); break}
+        }
+      } catch {
+        encryptedInNums.push(letter);
       }
+    };
+
+    let dublEncryptedKey = [];
+
+    for (let index = 1; index <= Math.ceil(encryptedInNums.length / encryptedKey.length); index++) {
+      dublEncryptedKey = dublEncryptedKey.concat(encryptedKey);
+    }
+console.log(encryptedInNums);
+    for (let o = 0; o < encryptedInNums.length; o++) {
+      if(encryptedInNums[o] == ' '){encryptedInSum.push(encryptedInNums[o]); continue};
+      if (regexSymbol.test(encryptedInNums[o])){encryptedInSum.push(encryptedInNums[o]); continue};
+
+      if (Number(encryptedInNums[o]) + Number(dublEncryptedKey[o]) <= 26){
+        encryptedInSum.push(Number(encryptedInNums[o]) + Number(dublEncryptedKey[o]));
+      } else if (Number(encryptedInNums[o]) + Number(dublEncryptedKey[o]) > 26){
+        encryptedInSum.push((Number(encryptedInNums[o]) + Number(dublEncryptedKey[o])) % 26);
+      };
     }
 
+    console.log(encryptedInSum);
+
+    let finalCipher = [];
+
+    for (const iter of encryptedInSum) {
+      if(iter == ' '){finalCipher.push(iter);};
+      if (regexSymbol.test(iter)){finalCipher.push(iter);};
+      try{
+        for (let i = 0; i < objKey.length; i++) {
+          if (iter === +objKey[i]){finalCipher.push(cipherK[i]); break};
+        }
+      }
+      catch {finalCipher.push(iter); break};
+    }
+    console.log(finalCipher);
+
+    return finalCipher.join().toUpperCase();
   }
   decrypt(str, key) {
-    if (str === null || key === null){throw new Error('Incorrect arguments!')};
+    if (str == null || key == null){throw new Error('Incorrect arguments!')};
 
   }
 }
+
+const directMachine = new VigenereCipheringMachine();
+
+const reverseMachine = new VigenereCipheringMachine(false);
+
+directMachine.encrypt('attack at dawn!', 'alphonse');
+
 
 module.exports = {
   VigenereCipheringMachine
